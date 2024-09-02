a36d691982fc69aeee47810c2eb6d8eac5f14787
banushasarma <sharmabanu03@gmail.com>
2024-09-02 20:51:51 +0530

    QuestionController completed

--- src/main/java/com/telesko/quizapp/controller/QuestionController.java
+++ src/main/java/com/telesko/quizapp/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.telesko.quizapp.controller;
 import com.telesko.quizapp.model.Question;
 import com.telesko.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,19 +17,18 @@ public class QuestionController {
     QuestionService questionService; // Autowire the QuestionService
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions() {
+    public ResponseEntity<List<Question>> getAllQuestions() {
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category) {
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
         return questionService.getQuestionsByCategory(category);
     }
 
     //Add the Question
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question) {
-        questionService.addQuestion(question);
-        return "success";
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
     }
 }
