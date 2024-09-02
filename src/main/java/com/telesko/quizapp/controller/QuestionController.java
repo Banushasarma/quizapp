346c51386befcd6183ea187ab54ad1a60cc0994c
banushasarma <sharmabanu03@gmail.com>
2024-09-02 19:44:28 +0530

    some modifications

--- src/main/java/com/telesko/quizapp/controller/QuestionController.java
+++ src/main/java/com/telesko/quizapp/controller/QuestionController.java
@@ -3,13 +3,11 @@ package com.telesko.quizapp.controller;
 import com.telesko.quizapp.model.Question;
 import com.telesko.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
- // Import the QuestionService class
+// Import the QuestionService class
 
 @RestController
 @RequestMapping("question")
@@ -20,5 +18,17 @@ public class QuestionController {
     @GetMapping("allQuestions")
     public List<Question> getAllQuestions() {
         return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category) {
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    //Add the Question
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question) {
+        questionService.addQuestion(question);
+        return "success";
     }
 }
--- src/main/java/com/telesko/quizapp/service/QuestionService.java
+++ src/main/java/com/telesko/quizapp/service/QuestionService.java
@@ -16,4 +16,12 @@ public class QuestionService {
     public List<Question> getAllQuestions() {
         return questionDao.findAll();
     }
+
+    public List<Question> getQuestionsByCategory(String category) {
+        return questionDao.findByCategory(category);
+    }
+
+    public void addQuestion(Question question) {
+        questionDao.save(question);
+    }
 }
